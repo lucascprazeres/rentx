b20d7f4d07393f9223347ad555d0443b7ab14135
Lucas Prazeres <olucascostadp@gmail.com>
2021-03-16 22:01:40 -0300

    feat(specification): add specification creation

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,27 @@
+import { Specification } from "../models/Specification";
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationDTO,
+} from "./ISpecificationsRepository";
+
+export class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  async findByName(name: string): Promise<Specification | undefined> {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = new Specification();
+
+    Object.assign(specification, { name, description });
+
+    this.specifications.push(specification);
+  }
+}
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", async (request, response) => {
+  try {
+    const { name, description } = request.body;
+
+    const createSpecification = new CreateSpecificationService(
+      specificationsRepository
+    );
+
+    await createSpecification.execute({ name, description });
+
+    return response.status(201).send();
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export { specificationsRoutes };
