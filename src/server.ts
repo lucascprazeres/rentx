a4043b071f4f339f2e528ee87a3e78341563dc6d
Lucas Prazeres <olucascostadp@gmail.com>
2021-03-19 14:39:09 -0300

    docs(categories): add swagger ui and documentates the create and list routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 
 export default app;
