52f04947042f4a6cf7661c714492622ffc5b2f33
Lucas Prazeres <olucascostadp@gmail.com>
2021-03-30 07:05:46 -0300

    feat: add user creation

---
+++ src/modules/accounts/repositories/implementations/UsersRespository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+  async create({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
