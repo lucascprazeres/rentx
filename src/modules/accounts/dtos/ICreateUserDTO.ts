027700274865853cdb11ca5c41c8b83bd7314509
Lucas Prazeres <olucascostadp@gmail.com>
2021-03-31 11:23:42 -0300

    ref(users): remove property username from user creation

--- src/modules/accounts/dtos/ICreateUserDTO.ts
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -1,7 +1,6 @@
 export interface ICreateUserDTO {
   name: string;
   email: string;
-  username: string;
   password: string;
   driver_license: string;
 }
--- src/modules/accounts/repositories/implementations/UsersRespository.ts
+++ src/modules/accounts/repositories/implementations/UsersRespository.ts
@@ -12,14 +12,12 @@ export class UsersRepository implements IUsersRepository {
   }
   async create({
     name,
-    username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       password,
       driver_license,
