0243cbe21df186e81f7a6a80f999e75858669999
Lucas Prazeres <olucascostadp@gmail.com>
2021-04-15 07:42:34 -0300

    ref(database): add database name argument in createConnection

--- src/shared/infra/typeorm/seeds/admin.ts
+++ src/shared/infra/typeorm/seeds/admin.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import createConnection from "../connection";
 
 (async () => {
-  const connection = await createConnection();
+  const connection = await createConnection("localhost");
 
   const id = uuidv4();
   const password = await hash("admin", 8);
@@ -13,4 +13,6 @@ import createConnection from "../connection";
     `INSERT INTO USERS(id, name, email, password, admin, driver_license, created_at) 
     values ('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'admin_license', now())`
   );
+
+  await connection.close();
 })().then(() => console.log("> admin created successfully"));
