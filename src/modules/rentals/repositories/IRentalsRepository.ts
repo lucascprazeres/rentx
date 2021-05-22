3893d8daeb340cbf007defe1ba22f002819954b3
Lucas Prazeres <olucascostadp@gmail.com>
2021-05-22 08:05:13 -0300

    feat(rentals): add rental listing by user

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -6,5 +6,6 @@ export interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>;
   findOpenRentalByCarId(id: string): Promise<Rental | undefined>;
   findOpenRentalByUserId(id: string): Promise<Rental | undefined>;
+  findAllByUserId(id: string): Promise<Rental[]>;
   findById(id: string): Promise<Rental>;
 }
---
+++ src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUserUseCase.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListRentalsByUserUseCase {
+  constructor(
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository
+  ) {}
+
+  async execute({ user_id }: IRequest): Promise<Rental> {
+    const rentals = await this.rentalsRepository.findAllByUserId(user_id);
+
+    return rentals;
+  }
+}
+
+export { ListRentalsByUserUseCase };
